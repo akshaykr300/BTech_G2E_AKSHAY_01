27f86af8bff4d6e244a1001d6596bd45e847db5d
akshaykr300 <25116cn360@gmail.com>
2026-03-23 21:12:03 +0530

    Add files via upload

---
+++ calc.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    char op;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("Enter operator (+, -, *, /): ");
+    scanf(" %c", &op);   // space before %c to avoid input issue
+
+    switch(op) {
+        case '+':
+            printf("Result = %d", a + b);
+            break;
+
+        case '-':
+            printf("Result = %d", a - b);
+            break;
+
+        case '*':
+            printf("Result = %d", a * b);
+            break;
+
+        case '/':
+            if(b != 0)
+                printf("Result = %d", a / b);
+            else
+                printf("Division by zero not allowed!");
+            break;
+
+        default:
+            printf("Invalid operator!");
+    }
+
+    return 0;
+}
